36fd598e51fdc6960e03b140f583980224e97e58
ZhouQin <zhouqin@idcos.com>
2016-06-01 09:27:55 +0800

    extract: argparse

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+import argparse
+
 
 def args(*args, **kwargs):
     def _decorator(func):
@@ -24,3 +26,28 @@ def func_args(func, match_args):
         fn_args.append(getattr(match_args, arg))
 
     return fn_args
+    
+
+def parse_command(ENHANCES=[]):
+    top_parser = argparse.ArgumentParser(prog='top')
+    subparsers = top_parser.add_subparsers()
+
+    for enhance in ENHANCES:
+
+        command_object = ENHANCES[enhance]()
+        command_parser = subparsers.add_parser(enhance)
+        command_parser.set_defaults(command_object=command_object)
+        command_subparsers = command_parser.add_subparsers(dest='action')
+        for (action, action_fn) in methods_of(command_object):
+            parser = command_subparsers.add_parser(action)
+            action_kwargs = []
+            for args, kwargs in getattr(action_fn, 'args', []):
+                parser.add_argument(*args, **kwargs)
+
+            parser.set_defaults(action_fn=action_fn)
+            parser.set_defaults(action_kwargs=action_kwargs)
+
+    match_args = top_parser.parse_args()
+    fn = match_args.action_fn
+    fn_args = func_args(fn, match_args)
+    return fn, fn_args
--- git-x.py
+++ git-x.py
@@ -2,12 +2,10 @@
 # coding:utf-8
 
 import requests
-import commands
-import argparse
 from prettytable import PrettyTable
 import re
 from gitlabConfig import GitlabConfig
-from common import args, methods_of, func_args
+from common import args, parse_command
 
 
 class IssuesCommand(object):
@@ -46,27 +44,6 @@ ENHANCES = {
 
 
 if __name__ == '__main__':
-
-    top_parser = argparse.ArgumentParser(prog='top')
-    subparsers = top_parser.add_subparsers()
-
-    for enhance in ENHANCES:
-
-        command_object = ENHANCES[enhance]()
-        command_parser = subparsers.add_parser(enhance)
-        command_parser.set_defaults(command_object=command_object)
-        command_subparsers = command_parser.add_subparsers(dest='action')
-        for (action, action_fn) in methods_of(command_object):
-            parser = command_subparsers.add_parser(action)
-            action_kwargs = []
-            for args, kwargs in getattr(action_fn, 'args', []):
-                parser.add_argument(*args, **kwargs)
-
-            parser.set_defaults(action_fn=action_fn)
-            parser.set_defaults(action_kwargs=action_kwargs)
-
-    match_args = top_parser.parse_args()
-    fn = match_args.action_fn
-    fn_args = func_args(fn, match_args)
+    fn, fn_args = parse_command(ENHANCES)
     fn(*fn_args)
     # it works: python git-x.py issues list
